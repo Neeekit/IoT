1ef4b6f9fddb9fd36e781c53c6737ae6615df54b
Ilia-Pashkin <52559410+Ilia-Pashkin@users.noreply.github.com>
2020-06-06 03:56:57 +0300

    Add files via upload

---
+++ front/models.py
@@ -0,0 +1,29 @@
+from app import db
+from datetime import datetime
+from flask_user import UserMixin
+import uuid
+ 
+class Machine(db.Model):
+	__tablename__ = 'machines'
+	id = db.Column(db.Integer, primary_key=True)
+	adress = db.Column(db.Text(), nullable=False, server_default='')
+	code = db.Column(db.Text(), nullable=False, server_default='')
+	cells_count = db.Column(db.Integer(), nullable=False, server_default='0')
+
+class Book(db.Model):
+	__tablename__ = 'books'
+	id = db.Column(db.Integer, primary_key=True)
+	image_path = db.Column(db.Text(), nullable=False, server_default='')
+	cell_number = db.Column(db.Integer(), nullable=False, server_default='0')
+	machine_id = db.Column(db.Integer(), db.ForeignKey('machines.id'))
+	machine = db.relationship('Machine', backref=db.backref('books', lazy=True))
+
+class BookOperations(db.Model):
+	__tablename__ = 'book_operations'
+	id = db.Column(db.Integer, primary_key=True)
+	operation_type = db.Column(db.Enum('in', 'out', name='operation_type'), nullable=False, server_default='in')
+	date_time = db.Column(db.DateTime(), nullable=False, default=datetime.utcnow)
+	book_id = db.Column(db.Integer(), db.ForeignKey('books.id'), nullable=True)
+	machine_id = db.Column(db.Integer(), db.ForeignKey('machines.id'))
+	cell_number = db.Column(db.Integer(), nullable=False, server_default='0')
+
